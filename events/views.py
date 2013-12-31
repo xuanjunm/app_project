96258a1acac4506a568edb4ec0e514fa98e79972
Sean Su <seansu@me.com>
2013-12-31 12:05:30 -0800

    remove all instances of MessageMixin

--- events/views.py
+++ events/views.py
@@ -6,8 +6,7 @@ from events.models import *
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.core.urlresolvers import reverse
-from events.forms import *
-from events.mixins import *
+#from events.mixins import *
 
 class EventsList(generic.ListView):
     template_name = 'events/events_list.html'
@@ -23,28 +22,26 @@ class EventDetailsView(generic.DetailView):
     model = Event
     template_name = 'events/event_details.html'
 
-class CreateEventView(MessageMixin, generic.CreateView):
+class CreateEventView(generic.CreateView):
     model = Event
-#    form_class = EventForm
+
     template_name = 'events/create_event.html'
-    success_message = 'Event has been created.'
+#    success_message = 'Event has been created.'
 
     def get_success_url(self):
         return reverse('events:events_list')
 
-class UpdateEventView(MessageMixin, generic.UpdateView):
+class UpdateEventView(generic.UpdateView):
     model = Event
 #    form_class = EventForm
     template_name = 'events/update_event.html'
-    success_message = 'Event has been updated.'
 
     def get_success_url(self):
         return reverse('events:event_details', args=(self.get_object().id,))
 
-class DeleteEventView(MessageMixin, generic.DeleteView):
+class DeleteEventView(generic.DeleteView):
     model = Event
     template_name = 'events/delete_event.html'
-    success_message = 'Event has been deleted.'
 
     def get_success_url(self):
         return reverse('events:events_list')
