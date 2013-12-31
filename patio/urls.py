fea882c0988226a0f5f58f516776cff7deae87e0
Sean Su <seansu@me.com>
2013-12-30 20:39:21 -0800

    login/logout implemented with user dashboard page

--- patio/urls.py
+++ patio/urls.py
@@ -1,7 +1,14 @@
+from . import views
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='patio/main.html'), 
         name='main'),
+    url(r'^login/$', 'django.contrib.auth.views.login',
+        {'template_name': 'patio/login.html'}, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout',
+        name='logout'),
+       # {'next_page': '/'}, name='logout'),
+    url(r'^dashboard/$', views.DashboardView.as_view(), name='dashboard'),
 )
--- patio/views.py
+++ patio/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from django.views import generic
+from django.contrib.auth.models import User
+from .models import *
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
+class DashboardView(generic.TemplateView):
+    template_name = 'patio/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+        user = UserProfile.objects.get(user=self.request.user.id)
+
+        context['user_profile'] = user
+        return context
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(DashboardView, self).dispatch(*args, **kwargs)
