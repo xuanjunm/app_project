bf354506b2017698815a7119b2f4cf1fe9ed14c1
Sean Su <seansu@me.com>
2014-05-07 14:49:24 -0700

    Changes
    - rename wsgi file

--- app_project/d_wsgi.py
+++ app_project/d_wsgi.py
@@ -2,11 +2,11 @@
 import os, sys
 
 # make sure app's modules can be found
-sys.path.append('/home/sean/app_project')
+sys.path.append('/srv/www/app_project')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'app_project.settings'
 
 # Switch to the directory of your project. (Optional.)
-# os.chdir("/home/sean/mysite")
+# os.chdir("/srv/www/mysite")
 
 from django.contrib.auth.handlers.modwsgi import check_password
 
